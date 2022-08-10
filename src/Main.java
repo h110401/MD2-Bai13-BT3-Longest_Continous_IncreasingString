edea1d732cd7e27bdfb8cb3666cbe0a915ffbf8a
h110401 <hung110401@gmail.com>
2022-08-11 02:56:54 +0700

    fix

--- src/Main.java
+++ src/Main.java
@@ -2,59 +2,28 @@ import java.util.*;
 
 
 public class Main {
-
-    public Main() {
-        Scanner sc = new Scanner(System.in);
-        String string = sc.nextLine();
-
-//        int[] ascii = new int[string.length()];
-//        for (int i = 0; i < string.length(); i++) {
-//            ascii[i] = string.charAt(i);
-//        }
-//
-//        System.out.println(Arrays.toString(ascii));
-
-        Set<String> increasingStrings = new HashSet<>();
-
-
-        for (int i = 0; i < string.length(); i++) {
-            List<StringBuilder> temp = new ArrayList<>();
-            temp.add(new StringBuilder().append(string.charAt(i)));
-            for (int j = i; j < string.length(); j++) {
-                List<StringBuilder> holder = new ArrayList<>();
-                holder.add(new StringBuilder().append(string.charAt(j)));
-                for (StringBuilder stringBuilder : temp) {
-                    if (string.charAt(j) > stringBuilder.charAt(stringBuilder.length() - 1)) {
-                        holder.add(new StringBuilder().append(stringBuilder).append(string.charAt(j)));
-                    } else {
-                        StringBuilder holdTemp = new StringBuilder();
-                        holdTemp.append(stringBuilder);
-                        while (string.charAt(j) <= stringBuilder.charAt(stringBuilder.length() - 1) && holdTemp.length() > 0) {
-                            holdTemp.deleteCharAt(holdTemp.length() - 1);
-                        }
-                        holder.add(holdTemp.append(string.charAt(j)));
-                    }
-                }
-                temp.addAll(holder);
-            }
-            for (StringBuilder s : temp) {
-                increasingStrings.add(s.toString());
-            }
-        }
-
-        int maxLength = 0;
-        for (String s : increasingStrings) {
-            if (s.length() > maxLength) {
-                maxLength = s.length();
-            }
-        }
-        int finalMaxLength = maxLength;
-        increasingStrings.removeIf(s -> s.length() < finalMaxLength);
-        System.out.println(increasingStrings);
-        new Main();
-    }
     public static void main(String[] args) {
-        new Main();
+        String string = "abcabcdgabxy";
+        List<Character> current = new ArrayList<>();
+        List<Character> max = new ArrayList<>();
+        for (int i = 0; i < string.length(); i++) {
+            for (int j = 0; j < string.length(); j++) {
+                if (max.isEmpty()) {
+                    current.add(string.charAt(j));
+                    max.add(string.charAt(j));
+                    continue;
+                }
+                if (string.charAt(j) < current.get(current.size() - 1)) {
+                    current.clear();
+                }
+                current.add(string.charAt(j));
+                if (current.size() > max.size()) {
+                    max.clear();
+                    max.addAll(current);
+                }
+            }
+        }
+        System.out.println(max);
     }
 }
 
