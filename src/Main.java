111b78debe5d5234b1c2a58686a445f549151ba3
h110401 <hung110401@gmail.com>
2022-08-10 16:24:20 +0700

    update

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,60 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+
 
 public class Main {
-    public static void main(String[] args) {
-        String string = "abcabcdgabxy";
-        List<Character> current = new ArrayList<>();
-        List<Character> max = new ArrayList<>();
-        for (Character c : string.toCharArray()) {
-            if (max.isEmpty()) {
-                current.add(c);
-                max.add(c);
-                continue;
-            }
-            if (c.compareTo(current.get(current.size() - 1)) < 0) {
-                current.clear();
-            }
-            current.add(c);
-            if (current.size() > max.size()) {
-                max.clear();
-                max.addAll(current);
-            }
 
+    public Main() {
+        Scanner sc = new Scanner(System.in);
+        String string = sc.nextLine();
+
+//        int[] ascii = new int[string.length()];
+//        for (int i = 0; i < string.length(); i++) {
+//            ascii[i] = string.charAt(i);
+//        }
+//
+//        System.out.println(Arrays.toString(ascii));
+
+        Set<String> increasingStrings = new HashSet<>();
+
+
+        for (int i = 0; i < string.length(); i++) {
+            List<StringBuilder> temp = new ArrayList<>();
+            temp.add(new StringBuilder().append(string.charAt(i)));
+            for (int j = i; j < string.length(); j++) {
+                List<StringBuilder> holder = new ArrayList<>();
+                holder.add(new StringBuilder().append(string.charAt(j)));
+                for (StringBuilder stringBuilder : temp) {
+                    if (string.charAt(j) > stringBuilder.charAt(stringBuilder.length() - 1)) {
+                        holder.add(new StringBuilder().append(stringBuilder).append(string.charAt(j)));
+                    } else {
+                        StringBuilder holdTemp = new StringBuilder();
+                        holdTemp.append(stringBuilder);
+                        while (string.charAt(j) <= stringBuilder.charAt(stringBuilder.length() - 1) && holdTemp.length() > 0) {
+                            holdTemp.deleteCharAt(holdTemp.length() - 1);
+                        }
+                        holder.add(holdTemp.append(string.charAt(j)));
+                    }
+                }
+                temp.addAll(holder);
+            }
+            for (StringBuilder s : temp) {
+                increasingStrings.add(s.toString());
+            }
         }
-        System.out.println(max);
+
+        int maxLength = 0;
+        for (String s : increasingStrings) {
+            if (s.length() > maxLength) {
+                maxLength = s.length();
+            }
+        }
+        int finalMaxLength = maxLength;
+        increasingStrings.removeIf(s -> s.length() < finalMaxLength);
+        System.out.println(increasingStrings);
+        new Main();
+    }
+    public static void main(String[] args) {
+        new Main();
     }
 }
 
